7ab6e3156c5c1f9cc6b563c076a15f1b734ea92c
arcolife <arcshams@gmail.com>
2014-08-04 09:02:07 +0530

    binary file load status prints

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,6 @@ app = Flask(__name__)
 # Configurations
 app.config.from_object('config')
 
+print "Loading Binary vector file. Please wait...."
 from sentence_relate_mikolov import verySimpleModelMikolov
+print "File loaded!"
